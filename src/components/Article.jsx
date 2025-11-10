7f3041d3c809f8d538a312da52c14d4cd58a0f63
srmacpherson <srmacpherson96@gmail.com>
2025-11-10 14:25:33 +0000

    adjust article time to be a human-readable format and removed Comments component from Article component

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,16 +1,17 @@
-import Comments from "./Comments";
-
 function Article( {article} ) {
+
+  const isoString = article.created_at;
+  const date = new Date(isoString)
+
     return (
         <>
-          <p>{article.title}</p>
+          <h2>{article.title}</h2>
           <p>Topic: {article.topic}</p>
-          <p>{article.created_at}</p>
+          <p>{date.toLocaleString().slice(0, -3)}</p>
           <p>Author: {article.author}</p>
           <img src={article.article_img_url}/>
           <p>Votes: {article.votes}</p>
           <p>Comments: {article.comment_count}</p>
-          <Comments/>
         </>
     )
 }
