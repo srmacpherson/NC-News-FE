7946fcb54c0c115bbe58338d21cf5d732c4f6779
srmacpherson <srmacpherson96@gmail.com>
2025-11-12 15:06:34 +0000

    format basic navigation links for the sidebar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router";
+
 function Navigation() {
-    return <p>Navigation</p>
+  return (
+    <>
+      <Link to="/articles">
+        <h3>ALL</h3>
+      </Link>
+      <Link to="/articles">
+        <h3>TRENDING (not done yet)</h3>
+      </Link>
+      <Link to="/topics">
+        <h3>TOPICS</h3>
+      </Link>
+    </>
+  );
 }
 
 export default Navigation;
