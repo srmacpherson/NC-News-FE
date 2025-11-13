5bf84c150a6d016efd735ee4f748e452753ee75c
srmacpherson <srmacpherson96@gmail.com>
2025-11-13 12:17:25 +0000

    useEffect now takes place in the Content component instead of the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Routes, Route, useSearchParams } from "react-router";
-import axios from "axios";
 import Header from "./components/Header";
 import SideBar from "./components/SideBar";
 import Content from "./components/Content";
@@ -10,45 +9,34 @@ import Topic from "./components/Topic";
 import "./App.css";
 
 function App() {
-  const [articles, setArticles] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
   const [searchParams, setSearchParams] = useSearchParams();
+  const [articles, setArticles] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get(
-        `https://nc-news-be-vwd3.onrender.com/api/articles?order=${
-          Object.fromEntries(searchParams.entries()).order
-            ? Object.fromEntries(searchParams.entries()).order
-            : "desc"
-        }&sort_by=${
-          Object.fromEntries(searchParams.entries()).sort_by
-            ? Object.fromEntries(searchParams.entries()).sort_by
-            : ""
-        }`
-      )
-      .then((res) => {
-        setArticles(res.data.articles);
-      })
-      .catch((err) => {
-        setError(err);
-        console.error(error);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, [searchParams]);
-
-  if (error) return <p>Something went wrong.</p>;
-  if (isLoading) return <p>Loading...</p>;
   return (
     <>
       <Header />
       <SideBar setSearchParams={setSearchParams} />
       <Routes>
-        <Route path="/" element={<Content articles={articles} />} />
-        <Route path="/articles" element={<Content articles={articles} />} />
+        <Route
+          path="/"
+          element={
+            <Content
+              articles={articles}
+              setArticles={setArticles}
+              searchParams={searchParams}
+            />
+          }
+        />
+        <Route
+          path="/articles"
+          element={
+            <Content
+              articles={articles}
+              setArticles={setArticles}
+              searchParams={searchParams}
+            />
+          }
+        />
         <Route path="/articles/:article_id" element={<Spotlight />} />
         <Route path="/topics" element={<Topics />} />
         <Route path="/topics/:topic" element={<Topic articles={articles} />} />
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,8 +1,41 @@
 import Article from "./Article";
 import Pagination from "./Pagination";
 import { Link } from "react-router";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function Content({ articles }) {
+function Content({ searchParams, articles, setArticles }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://nc-news-be-vwd3.onrender.com/api/articles?order=${
+          Object.fromEntries(searchParams.entries()).order
+            ? Object.fromEntries(searchParams.entries()).order
+            : "desc"
+        }&sort_by=${
+          Object.fromEntries(searchParams.entries()).sort_by
+            ? Object.fromEntries(searchParams.entries()).sort_by
+            : ""
+        }`
+      )
+      .then((res) => {
+        setArticles(res.data.articles);
+      })
+      .catch((err) => {
+        setError(err);
+        console.error(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [searchParams]);
+
+  if (error) return <p>Something went wrong.</p>;
+  if (isLoading) return <p>Loading...</p>;
+
   return (
     <section className="content-section">
       <h3>Articles</h3>
